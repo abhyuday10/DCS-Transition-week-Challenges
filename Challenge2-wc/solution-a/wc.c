f6527d0290bcc4e4cf136dbf302a306d347e7499
Abhyuday <abhyuday10@gmail.com>
2020-10-05 14:32:23 +0100

    bug

--- Challenge2-wc/solution-a/wc.c
+++ Challenge2-wc/solution-a/wc.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+
+// shakespeare should output: 124455  901325 shakespeare.txt
 int main(int argc, char *argv[])
 {
-    int lines;
-    int words;
+    int lines = 0;
+    int words = 0;
     FILE *fp = fopen(argv[1], "r");
 
-    int ch;
+    int ch = 0;
+    int chprev = 'z';
     while (!feof(fp))
     {
         ch = fgetc(fp);
-        if (ch == '\n')
+        if (ch == '\n' || ch == '\0')
         {
             lines++;
         }
-        else if (ch == ' ')
+        if (ch != chprev && (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0'))
         {
             words++;
         }
+        chprev = ch;
     }
     printf("%d %d %s\n", lines, words, argv[1]);
     return 0;

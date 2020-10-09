4f291137a396dcc50330f262597a2f17a9b6fd48
Abhyuday <abhyuday10@gmail.com>
2020-10-09 14:09:35 +0100

    formatting

--- Challenge-chess/src/main/java/uk/ac/warwick/dcs/chess/piece/King.java
+++ Challenge-chess/src/main/java/uk/ac/warwick/dcs/chess/piece/King.java
@@ -1,9 +1,12 @@
 package uk.ac.warwick.dcs.chess.piece;
+
 import uk.ac.warwick.dcs.chess.*;
+
 public class King extends ChessPiece {
     private static final long serialVersionUID = 1L;
+
     public King(Board board, boolean isWhite, int vertical, int horizontal) {
-        super(board, isWhite, (char)((int)ChessIcons.W_KING + (isWhite ? 0 : 6)), vertical, horizontal);
+        super(board, isWhite, (char) ((int) ChessIcons.W_KING + (isWhite ? 0 : 6)), vertical, horizontal);
     }
 
     @Override
